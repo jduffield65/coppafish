388e176e033238d6d1181d33ecdc2d3a56427246
jduffield65 <jduffield65@gmail.com>
2022-06-20 16:53:45 +0100

    Update matlab.py
    Now saves less data when converting to matlab - does initial thresholding.

--- iss/utils/matlab.py
+++ iss/utils/matlab.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import io
 from typing import Union, List
 from ..setup.notebook import Notebook
+from ..call_spots.base import quality_threshold
 
 
 def load_v_less_7_3(file_name: str, var_names: Union[str, List[str]]) -> Union[tuple, np.ndarray]:
@@ -80,7 +81,43 @@ def load_cell(file_name: str, var_name: str) -> list:
     return data
 
 
-def save_nb_results(nb: Notebook, file_name: str):
+def update_dict(nbp, dict, score_thresh):
+    """
+    Used in save_nb_results to reduced amount of data saved. Only data with score > score_thresh kept.
+    Args:
+        nbp:
+        dict:
+        score_thresh:
+
+    Returns:
+
+    """
+    pf = nbp.name + '_'
+    if pf != 'omp_' and pf != 'ref_spots_':
+        raise ValueError("Wrong page given, should be 'omp' or 'ref_spots'")
+    nbp.finalized = False
+    score_thresh_old = nbp.score_thresh
+    del nbp.score_thresh
+    nbp.score_thresh = score_thresh
+    keep = quality_threshold(nbp)
+    del nbp.score_thresh
+    nbp.score_thresh = score_thresh_old
+    nbp.finalized = True
+    for var in ['local_yxz', 'tile', 'colors', 'intensity', 'background_coef', 'gene_no']:
+        dict[pf + var] = dict[pf + var][keep]
+    if pf == 'ref_spots_':
+        del dict[pf + 'background_coef']
+        for var in ['isolated', 'score', 'score_diff']:
+            dict[pf + var] = dict[pf + var][keep]
+    if pf == 'omp_':
+        for var in ['shape_spot_local_yxz', 'shape_spot_gene_no', 'spot_shape_float']:
+            del dict[pf + var]
+        for var in ['coef', 'n_neighbours_pos', 'n_neighbours_neg']:
+            dict[pf + var] = dict[pf + var][keep]
+    return dict
+
+
+def save_nb_results(nb: Notebook, file_name: str, score_thresh_ref_spots = 0.15, score_thresh_omp=0.15):
     """
     Saves important information in notebook as a .mat file so can load in MATLAB and plot
     using python_testing/iss_object_from_python.m scripts
@@ -108,12 +145,14 @@ def save_nb_results(nb: Notebook, file_name: str):
         # Give 'ref_spots' prefix to key names as same variables in omp page.
         ref_spots_dict = {ref_spots_dict['PAGEINFO'] + '_' + k: v for k, v in ref_spots_dict.items() if not '___' in k}
         del ref_spots_dict['ref_spots_PAGEINFO']
+        ref_spots_dict = update_dict(nb.ref_spots, ref_spots_dict, score_thresh_ref_spots)
         mdic.update(ref_spots_dict)
 
     if nb.has_page('omp'):
         omp_dict = nb.omp.to_serial_dict()
         omp_dict = {omp_dict['PAGEINFO'] + '_' + k:v for k,v in omp_dict.items() if not '___' in k}
         del omp_dict['omp_PAGEINFO']
+        omp_dict = update_dict(nb.omp, omp_dict, score_thresh_omp)
         mdic.update(omp_dict)
     for k, v in mdic.items():
         if v is None:
