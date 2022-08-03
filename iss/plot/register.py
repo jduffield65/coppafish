021b795b74addb9c77bc8955fc5bf5be24d8a9e0
jduffield65 <jduffield65@gmail.com>
2022-08-03 19:53:58 +0100

    Add view_stitch_point_clouds

--- iss/plot/register.py
+++ iss/plot/register.py
@@ -3,12 +3,9 @@ from ..stitch import compute_shift
 from ..find_spots import spot_yxz, get_isolated_points
 from ..pcr import get_single_affine_transform
 from ..spot_colors.base import apply_transform
-from .stitch import view_shifts
+from .stitch import view_shifts, view_point_clouds
 from ..setup import Notebook
-from typing import Optional, List
-import matplotlib.pyplot as plt
-from matplotlib.widgets import TextBox, RadioButtons
-from scipy.spatial import KDTree
+from typing import Optional
 
 
 def view_initial_shift(nb: Notebook, t: int, r: int, c: Optional[int] = None,
@@ -73,158 +70,6 @@ def view_initial_shift(nb: Notebook, t: int, r: int, c: Optional[int] = None,
                 debug_info['scores_3d'], shift, shift_score_thresh, title, show)
     if return_shift:
         return shift
-
-
-class view_point_clouds:
-    def __init__(self, point_clouds: List, pc_labels: List, neighb_dist_thresh: float = 5, z_scale: float = 1,
-                 super_title: Optional[str] = None):
-        """
-        Plots two point clouds. `point_clouds[0]` always plotted but you can change the second point cloud using
-        radio buttons.
-
-        Args:
-            point_clouds: List of point clouds, each of which is yxz coordinates `float [n_points x 3]`.
-                `point_clouds[0]` is always plotted.
-                YX coordinates are in units of yx pixels. Z coordinates are in units of z pixels.
-                Radio buttons used to select other point_cloud plotted.
-            pc_labels: List of labels to appear in legend/radio-buttons for each point cloud.
-                Must provide one for each `point_cloud`.
-            neighb_dist_thresh: If distance between neighbours is less than this, a white line will connect them.
-            z_scale: `pixel_size_z / pixel_size_y` i.e. used to convert z coordinates from z-pixels to yx pixels.
-            super_title: Optional title for plot
-        """
-        n_point_clouds = len(point_clouds)
-        if len(point_clouds) != len(pc_labels):
-            raise ValueError(f'There are {n_point_clouds} but {len(pc_labels)} labels')
-        self.fig, self.ax = plt.subplots(1, 1)
-        subplots_adjust = [0.07, 0.775, 0.095, 0.89]
-        self.fig.subplots_adjust(left=subplots_adjust[0], right=subplots_adjust[1], bottom=subplots_adjust[2],
-                                 top=subplots_adjust[3])
-
-        # Find neighbours between all point clouds and the first.
-        self.neighb = [None]
-        n_matches_str = 'Number Of Matches'
-        for i in range(1, n_point_clouds):
-            tree = KDTree(point_clouds[i] * [1, 1, z_scale])
-            self.neighb = self.neighb + [tree.query(point_clouds[0] * [1, 1, z_scale],
-                                                    distance_upper_bound=neighb_dist_thresh)[1]]
-            self.neighb[i][self.neighb[i] == len(point_clouds[i])] = -1  # set inf distance neighb_ind to -1.
-            n_matches_str = n_matches_str + f'\n- {pc_labels[i]}: {np.sum(self.neighb[i] >= 0)}'
-
-        # Add text box to indicate number of matches between first point cloud and each of the others.
-        n_matches_ax = self.fig.add_axes([0.8, subplots_adjust[3] - 0.6, 0.15, 0.2])
-        plt.axis('off')
-        n_matches_ax.text(0.05, 0.95, n_matches_str)
-
-        #  round z to closest z-plane for plotting
-        for i in range(n_point_clouds):
-            point_clouds[i][:, 2] = np.rint(point_clouds[i][:, 2])
-        self.z_planes = np.arange(point_clouds[0][:, 2].min(), point_clouds[0][:, 2].max() + 1)
-        self.nz = len(self.z_planes)
-        self.z_ind = 0
-        self.z = self.z_planes[self.z_ind]
-        self.z_thick = 0
-        self.active_pc = [0, 1]
-        self.in_z = [np.array([val[:, 2] >= self.z - self.z_thick, val[:, 2] <= self.z + self.z_thick]).all(axis=0)
-                     for val in point_clouds]
-        self.point_clouds = point_clouds
-        self.pc_labels = np.array(pc_labels)
-        self.pc_shapes = ['rx', 'bo']
-        alpha = [1, 0.7]
-        self.pc_plots = [self.ax.plot(point_clouds[self.active_pc[i]][self.in_z[i], 1],
-                                      point_clouds[self.active_pc[i]][self.in_z[i], 0],
-                                      self.pc_shapes[i], label=self.pc_labels[i], alpha=alpha[i])[0]
-                         for i in range(2)]
-
-        self.neighb_yx = None
-        self.neighb_plot = None
-        self.update_neighb_lines()
-
-        self.ax.legend(loc='upper right')
-        self.ax.set_ylabel('Y')
-        self.ax.set_xlabel('X')
-        self.ax.set_ylim(np.clip(point_clouds[0][:, 0].min() - 5, 0, np.inf), point_clouds[0][:, 0].max() + 5)
-        self.ax.set_xlim(np.clip(point_clouds[0][:, 1].min() - 5, 0, np.inf), point_clouds[0][:, 1].max() + 5)
-
-        if self.nz > 1:
-            # If 3D, add text box to change number of z-planes collapsed onto single plane
-            # and add scrolling to change z-plane
-            self.ax.set_title(f'Z = {int(self.z)}', size=10)
-            self.fig.canvas.mpl_connect('scroll_event', self.z_scroll)
-            text_ax = self.fig.add_axes([0.8, 0.095, 0.15, 0.04])
-            self.text_box = TextBox(text_ax, 'Z-Thick', self.z_thick, color='k', hovercolor=[0.2, 0.2, 0.2])
-            self.text_box.cursor.set_color('r')
-            # change text box title to be above not to the left of box
-            label = text_ax.get_children()[0]  # label is a child of the TextBox axis
-            label.set_position([0.5, 2])  # [x,y] - change here to set the position
-            # centering the text
-            label.set_verticalalignment('top')
-            label.set_horizontalalignment('center')
-            self.text_box.on_submit(self.text_update)
-
-        if n_point_clouds >= 3:
-            # If 3 or more point clouds, add radio button to change the second point cloud shown.
-            buttons_ax = self.fig.add_axes([0.8, subplots_adjust[3] - 0.2, 0.15, 0.2])
-            self.buttons = RadioButtons(buttons_ax, self.pc_labels[1:], 0, activecolor='w')
-            for i in range(n_point_clouds-1):
-                self.buttons.circles[i].set_color('w')
-                self.buttons.circles[i].set_color('w')
-            self.buttons.set_active(0)
-            self.buttons.on_clicked(self.button_update)
-        if super_title is not None:
-            plt.suptitle(super_title, x=(0.07 + 0.775) / 2)
-        plt.show()
-
-    def z_scroll(self, event):
-        # Scroll to change current z-plane
-        if event.button == 'up':
-            self.z_ind = (self.z_ind + 1) % self.nz
-        else:
-            self.z_ind = (self.z_ind - 1) % self.nz
-        self.z = self.z_planes[self.z_ind]
-        self.update()
-
-    def text_update(self, text):
-        # Use text box to change number of z-planes shown on single plane.
-        # All points with z between self.z+/-self.z_thick will be shown.
-        self.z_thick = int(np.rint(float(text)))
-        self.update()
-
-    def button_update(self, selected_label):
-        # Use button to select the second point cloud to show.
-        self.active_pc[1] = np.where(self.pc_labels == selected_label)[0][0]
-        self.pc_plots[1].set_data(self.point_clouds[self.active_pc[1]][self.in_z[self.active_pc[1]], 1],
-                                  self.point_clouds[self.active_pc[1]][self.in_z[self.active_pc[1]], 0])
-        self.pc_plots[1].set_label(selected_label)
-        self.update_neighb_lines()
-        self.ax.legend(loc='upper right')
-        self.ax.figure.canvas.draw()
-
-    def update_neighb_lines(self):
-        # This updates the white lines connecting neighbours between point clouds.
-        for line in self.ax.get_lines():
-            # Remove all current connecting lines - know these are the only symbols with '-' linestyle.
-            if line.get_linestyle() == '-':
-                line.remove()
-        use_neighb = np.array([self.neighb[self.active_pc[1]] >= 0, self.in_z[0],
-                               self.in_z[self.active_pc[1]][self.neighb[self.active_pc[1]]]]).all(axis=0)
-        self.neighb_yx = [None, None]
-        for i in range(2):
-            self.neighb_yx[i] = \
-                np.vstack([self.point_clouds[0][use_neighb, i],
-                           self.point_clouds[self.active_pc[1]][self.neighb[self.active_pc[1]][use_neighb], i]])
-        self.neighb_plot = self.ax.plot(self.neighb_yx[1], self.neighb_yx[0], 'w', alpha=0.5, linewidth=1)
-
-    def update(self):
-        # This updates plot to point clouds / connecting lines according to current params.
-        self.in_z = [np.array([val[:, 2] >= self.z - self.z_thick, val[:, 2] <= self.z + self.z_thick]).all(axis=0)
-                     for val in self.point_clouds]
-        for i in range(2):
-            self.pc_plots[i].set_data(self.point_clouds[self.active_pc[i]][self.in_z[self.active_pc[i]], 1],
-                                      self.point_clouds[self.active_pc[i]][self.in_z[self.active_pc[i]], 0])
-        self.update_neighb_lines()
-        self.ax.set_title(f"Z = {int(self.z)}", size=10)
-        self.ax.figure.canvas.draw()
 
 
 def view_pcr(nb: Notebook, t: int, r: int, c: int):
@@ -295,3 +140,4 @@ def view_pcr(nb: Notebook, t: int, r: int, c: int):
         pc_labels = pc_labels + [f'Reference: r{r_ref}, c{c_ref} - Regularized']
     view_point_clouds(point_clouds, pc_labels, neighb_dist_thresh, z_scale,
                       f'Transform of tile {t} to round {r}, channel {c}')
+    plt.show()
--- iss/plot/stitch.py
+++ iss/plot/stitch.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
 import scipy.ndimage
+from matplotlib import pyplot as plt
+from matplotlib.widgets import TextBox, RadioButtons
+from scipy.spatial import KDTree
 from ..stitch import compute_shift, get_shifts_to_search
 from ..find_spots import spot_yxz
 from ..setup import Notebook
@@ -233,3 +236,217 @@ def view_stitch(nb: Notebook, t: int, direction: Optional[str] = None):
         plt.show()
     else:
         warnings.warn(f"Tile {t} has no overlapping tiles in nb.basic_info.use_tiles.")
+
+
+class view_point_clouds:
+    def __init__(self, point_clouds: List, pc_labels: List, neighb_dist_thresh: float = 5, z_scale: float = 1,
+                 super_title: Optional[str] = None):
+        """
+        Plots two point clouds. `point_clouds[0]` always plotted but you can change the second point cloud using
+        radio buttons.
+
+        Args:
+            point_clouds: List of point clouds, each of which is yxz coordinates `float [n_points x 3]`.
+                `point_clouds[0]` is always plotted.
+                YX coordinates are in units of yx pixels. Z coordinates are in units of z pixels.
+                Radio buttons used to select other point_cloud plotted.
+            pc_labels: List of labels to appear in legend/radio-buttons for each point cloud.
+                Must provide one for each `point_cloud`.
+            neighb_dist_thresh: If distance between neighbours is less than this, a white line will connect them.
+            z_scale: `pixel_size_z / pixel_size_y` i.e. used to convert z coordinates from z-pixels to yx pixels.
+            super_title: Optional title for plot
+        """
+        n_point_clouds = len(point_clouds)
+        if len(point_clouds) != len(pc_labels):
+            raise ValueError(f'There are {n_point_clouds} but {len(pc_labels)} labels')
+        self.fig, self.ax = plt.subplots(1, 1)
+        subplots_adjust = [0.07, 0.775, 0.095, 0.89]
+        self.fig.subplots_adjust(left=subplots_adjust[0], right=subplots_adjust[1], bottom=subplots_adjust[2],
+                                 top=subplots_adjust[3])
+
+        # Find neighbours between all point clouds and the first.
+        self.neighb = [None]
+        n_matches_str = 'Number Of Matches'
+        for i in range(1, n_point_clouds):
+            tree = KDTree(point_clouds[i] * [1, 1, z_scale])
+            self.neighb = self.neighb + [tree.query(point_clouds[0] * [1, 1, z_scale],
+                                                    distance_upper_bound=neighb_dist_thresh)[1]]
+            self.neighb[i][self.neighb[i] == len(point_clouds[i])] = -1  # set inf distance neighb_ind to -1.
+            n_matches_str = n_matches_str + f'\n- {pc_labels[i]}: {np.sum(self.neighb[i] >= 0)}'
+
+        # Add text box to indicate number of matches between first point cloud and each of the others.
+        n_matches_ax = self.fig.add_axes([0.8, subplots_adjust[3] - 0.6, 0.15, 0.2])
+        plt.axis('off')
+        n_matches_ax.text(0.05, 0.95, n_matches_str)
+
+        #  round z to closest z-plane for plotting
+        for i in range(n_point_clouds):
+            point_clouds[i][:, 2] = np.rint(point_clouds[i][:, 2])
+
+        # get yxz axis limits taking account all point clouds
+        pc_min_lims = np.zeros((n_point_clouds, 3))
+        pc_max_lims = np.zeros_like(pc_min_lims)
+        for i in range(n_point_clouds):
+            pc_min_lims[i] = np.min(point_clouds[i],axis=0)
+            pc_max_lims[i] = np.max(point_clouds[i],axis=0)
+
+        self.z_planes = np.arange(int(np.min(pc_min_lims[:, 2])), int(np.max(pc_max_lims[:, 2]) + 1))
+        self.nz = len(self.z_planes)
+        self.z_ind = 0
+        self.z = self.z_planes[self.z_ind]
+        self.z_thick = 0
+        self.active_pc = [0, 1]
+        self.in_z = [np.array([val[:, 2] >= self.z - self.z_thick, val[:, 2] <= self.z + self.z_thick]).all(axis=0)
+                     for val in point_clouds]
+        self.point_clouds = point_clouds
+        self.pc_labels = np.array(pc_labels)
+        self.pc_shapes = ['rx', 'bo']
+        alpha = [1, 0.7]
+        self.pc_plots = [self.ax.plot(point_clouds[self.active_pc[i]][self.in_z[i], 1],
+                                      point_clouds[self.active_pc[i]][self.in_z[i], 0],
+                                      self.pc_shapes[i], label=self.pc_labels[i], alpha=alpha[i])[0]
+                         for i in range(2)]
+
+        self.neighb_yx = None
+        self.neighb_plot = None
+        self.update_neighb_lines()
+
+        self.ax.legend(loc='upper right')
+        self.ax.set_ylabel('Y')
+        self.ax.set_xlabel('X')
+        self.ax.set_ylim(np.min(pc_min_lims[:, 0]), np.max(pc_max_lims[:, 0]))
+        self.ax.set_xlim(np.min(pc_min_lims[:, 1]), np.max(pc_max_lims[:, 1]))
+
+        if self.nz > 1:
+            # If 3D, add text box to change number of z-planes collapsed onto single plane
+            # and add scrolling to change z-plane
+            self.ax.set_title(f'Z = {int(self.z)}', size=10)
+            self.fig.canvas.mpl_connect('scroll_event', self.z_scroll)
+            text_ax = self.fig.add_axes([0.8, 0.095, 0.15, 0.04])
+            self.text_box = TextBox(text_ax, 'Z-Thick', self.z_thick, color='k', hovercolor=[0.2, 0.2, 0.2])
+            self.text_box.cursor.set_color('r')
+            # change text box title to be above not to the left of box
+            label = text_ax.get_children()[0]  # label is a child of the TextBox axis
+            label.set_position([0.5, 2])  # [x,y] - change here to set the position
+            # centering the text
+            label.set_verticalalignment('top')
+            label.set_horizontalalignment('center')
+            self.text_box.on_submit(self.text_update)
+
+        if n_point_clouds >= 3:
+            # If 3 or more point clouds, add radio button to change the second point cloud shown.
+            buttons_ax = self.fig.add_axes([0.8, subplots_adjust[3] - 0.2, 0.15, 0.2])
+            self.buttons = RadioButtons(buttons_ax, self.pc_labels[1:], 0, activecolor='w')
+            for i in range(n_point_clouds-1):
+                self.buttons.circles[i].set_color('w')
+                self.buttons.circles[i].set_color('w')
+            self.buttons.set_active(0)
+            self.buttons.on_clicked(self.button_update)
+        if super_title is not None:
+            plt.suptitle(super_title, x=(0.07 + 0.775) / 2)
+        # plt.show()
+
+    def z_scroll(self, event):
+        # Scroll to change current z-plane
+        if event.button == 'up':
+            self.z_ind = (self.z_ind + 1) % self.nz
+        else:
+            self.z_ind = (self.z_ind - 1) % self.nz
+        self.z = self.z_planes[self.z_ind]
+        self.update()
+
+    def text_update(self, text):
+        # Use text box to change number of z-planes shown on single plane.
+        # All points with z between self.z+/-self.z_thick will be shown.
+        self.z_thick = int(np.rint(float(text)))
+        self.update()
+
+    def button_update(self, selected_label):
+        # Use button to select the second point cloud to show.
+        self.active_pc[1] = np.where(self.pc_labels == selected_label)[0][0]
+        self.pc_plots[1].set_data(self.point_clouds[self.active_pc[1]][self.in_z[self.active_pc[1]], 1],
+                                  self.point_clouds[self.active_pc[1]][self.in_z[self.active_pc[1]], 0])
+        self.pc_plots[1].set_label(selected_label)
+        self.update_neighb_lines()
+        self.ax.legend(loc='upper right')
+        self.ax.figure.canvas.draw()
+
+    def update_neighb_lines(self):
+        # This updates the white lines connecting neighbours between point clouds.
+        for line in self.ax.get_lines():
+            # Remove all current connecting lines - know these are the only symbols with '-' linestyle.
+            if line.get_linestyle() == '-':
+                line.remove()
+        use_neighb = np.array([self.neighb[self.active_pc[1]] >= 0, self.in_z[0],
+                               self.in_z[self.active_pc[1]][self.neighb[self.active_pc[1]]]]).all(axis=0)
+        self.neighb_yx = [None, None]
+        for i in range(2):
+            self.neighb_yx[i] = \
+                np.vstack([self.point_clouds[0][use_neighb, i],
+                           self.point_clouds[self.active_pc[1]][self.neighb[self.active_pc[1]][use_neighb], i]])
+        self.neighb_plot = self.ax.plot(self.neighb_yx[1], self.neighb_yx[0], 'w', alpha=0.5, linewidth=1)
+
+    def update(self):
+        # This updates plot to point clouds / connecting lines according to current params.
+        self.in_z = [np.array([val[:, 2] >= self.z - self.z_thick, val[:, 2] <= self.z + self.z_thick]).all(axis=0)
+                     for val in self.point_clouds]
+        for i in range(2):
+            self.pc_plots[i].set_data(self.point_clouds[self.active_pc[i]][self.in_z[self.active_pc[i]], 1],
+                                      self.point_clouds[self.active_pc[i]][self.in_z[self.active_pc[i]], 0])
+        self.update_neighb_lines()
+        self.ax.set_title(f"Z = {int(self.z)}", size=10)
+        self.ax.figure.canvas.draw()
+
+
+def view_stitch_point_clouds(nb: Notebook, t: int, direction: str = 'south'):
+    # NOTE that directions should actually be 'north' and 'east'
+    if direction.lower() == 'south' or direction.lower() == 'west':
+        direction = direction.lower()
+    elif direction.lower() == 'north':
+        direction = 'south'
+    elif direction.lower() == 'east':
+        direction = 'west'
+    else:
+        raise ValueError(f"direction must be either 'south' or 'west' but {direction} given.")
+
+    direction_label = {'south': 'north', 'west': 'east'}  # label refers to actual direction
+    if direction == 'south':
+        t_neighb = np.where(np.sum(nb.basic_info.tilepos_yx == nb.basic_info.tilepos_yx[t, :] + [1, 0],
+                                   axis=1) == 2)[0]
+        if t_neighb not in nb.basic_info.use_tiles:
+            warnings.warn(f"Tile {t} has no overlapping tiles in the south direction so changing to west.")
+            direction = 'west'
+        else:
+            initial_shift = [-nb.basic_info.tile_sz, 0, 0]  # assuming no overlap between tiles
+            found_shift = nb.stitch.south_shifts[np.where(nb.stitch.south_pairs[:,0] == t)[0]][0]
+    if direction == 'west':
+        t_neighb = np.where(np.sum(nb.basic_info.tilepos_yx == nb.basic_info.tilepos_yx[t, :] + [0, 1],
+                                   axis=1) == 2)[0]
+        if t_neighb not in nb.basic_info.use_tiles:
+            raise ValueError(f"Tile {t} has no overlapping tiles in the west direction.")
+        initial_shift = [0, -nb.basic_info.tile_sz, 0]  # assuming no overlap between tiles
+        found_shift = nb.stitch.west_shifts[np.where(nb.stitch.west_pairs[:, 0] == t)[0]][0]
+
+    t_neighb = t_neighb[0]
+    r = nb.basic_info.ref_round
+    c = nb.basic_info.ref_channel
+    point_clouds = []
+    # add global coordinates of neighbour tile as point cloud that is always present.
+    point_clouds = point_clouds + [spot_yxz(nb.find_spots.spot_details, t_neighb, r, c) +
+                                   nb.stitch.tile_origin[t_neighb]]
+
+    local_yxz_t = spot_yxz(nb.find_spots.spot_details, t, r, c)
+    # Add point cloud for tile t assuming no overlap
+    point_clouds = point_clouds + [local_yxz_t + nb.stitch.tile_origin[t_neighb] + initial_shift]
+    # Add point cloud for tile t with found shift
+    point_clouds = point_clouds + [local_yxz_t + nb.stitch.tile_origin[t_neighb] + found_shift]
+    # Add point cloud for tile t in global coordinate system
+    point_clouds = point_clouds + [local_yxz_t + nb.stitch.tile_origin[t]]
+
+    config = nb.get_config()['stitch']
+    neighb_dist_thresh = config['neighb_dist_thresh']
+    z_scale = nb.basic_info.pixel_size_z / nb.basic_info.pixel_size_xy
+    pc_labels = [f'Tile {t_neighb}', f'Tile {t} - No overlap', f'Tile {t} - Shift', f'Tile {t} - Final']
+    view_point_clouds(point_clouds, pc_labels, neighb_dist_thresh, z_scale,
+                      f'Overlap between tile {t} and tile {t_neighb} in the {direction_label[direction]}')
+    plt.show()
