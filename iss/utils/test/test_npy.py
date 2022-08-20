88d37988a35ec7a97045d547251518d83df28df9
jduffield65 <jduffield65@gmail.com>
2022-08-20 18:53:51 +0100

    Update test_npy.py
    Deal with windows temp-file error

--- iss/utils/test/test_npy.py
+++ iss/utils/test/test_npy.py
@@ -66,48 +66,48 @@ class TestNPY(unittest.TestCase):
                         self.assertTrue((np.sum(loaded_image == invalid_value) == 0).all())
 
     def load_subset_all(self, r, channel, is_3d, tile_sz, yxz, apply_shift):
-        with tempfile.TemporaryDirectory() as tile_dir:
-            nbp_file, nbp_basic = get_notebook_pages(tile_dir, is_3d, tile_sz, self.Z_Scale, self.N_Rounds,
-                                                     self.N_Channels)
-            if not is_3d:
-                tile_sz = tile_sz[:2]
-                image = np.zeros((nbp_basic.n_channels,) + tuple(tile_sz), dtype=np.int32)
-                for c in range(nbp_basic.n_channels):
-                    image[c] = single_random_tile(nbp_basic, r, c, tile_sz)
-                utils.npy.save_tile(nbp_file, nbp_basic, image.copy(), self.t, r)
-                if isinstance(yxz, list):
-                    coord_index = np.ix_(yxz[0], yxz[1])
-                else:
-                    coord_index = tuple(np.asarray(yxz[:, i]) for i in range(2))
-                im_subset = image[channel][coord_index]
-            else:
-                image = single_random_tile(nbp_basic, r, channel, tile_sz)
-                utils.npy.save_tile(nbp_file, nbp_basic, image.copy(), self.t, r, channel)
-                if yxz[0] is None:
-                    # Case where want to load in entire z-plane
-                    im_subset = image[:, :, yxz[2]]
-                    if isinstance(yxz[2], list):
-                        im_subset = im_subset[:, :, 0]
-                else:
+        try:
+            with tempfile.TemporaryDirectory() as tile_dir:
+                nbp_file, nbp_basic = get_notebook_pages(tile_dir, is_3d, tile_sz, self.Z_Scale, self.N_Rounds,
+                                                         self.N_Channels)
+                if not is_3d:
+                    tile_sz = tile_sz[:2]
+                    image = np.zeros((nbp_basic.n_channels,) + tuple(tile_sz), dtype=np.int32)
+                    for c in range(nbp_basic.n_channels):
+                        image[c] = single_random_tile(nbp_basic, r, c, tile_sz)
+                    utils.npy.save_tile(nbp_file, nbp_basic, image.copy(), self.t, r)
                     if isinstance(yxz, list):
-                        coord_index = np.ix_(yxz[0], yxz[1], yxz[2])
-                        im_subset = image[coord_index]
-                        # check shape correct
-                        self.assertTrue(im_subset.shape == tuple([len(val) for val in yxz]))
+                        coord_index = np.ix_(yxz[0], yxz[1])
                     else:
-                        coord_index = tuple(np.asarray(yxz[:, i]) for i in range(3))
-                        im_subset = image[coord_index]
-                        # check shape correct
-                        self.assertTrue(im_subset.shape == yxz.shape[:1])
-            if not apply_shift:
-                im_subset = im_subset + nbp_basic.tile_pixel_value_shift
-            try:
+                        coord_index = tuple(np.asarray(yxz[:, i]) for i in range(2))
+                    im_subset = image[channel][coord_index]
+                else:
+                    image = single_random_tile(nbp_basic, r, channel, tile_sz)
+                    utils.npy.save_tile(nbp_file, nbp_basic, image.copy(), self.t, r, channel)
+                    if yxz[0] is None:
+                        # Case where want to load in entire z-plane
+                        im_subset = image[:, :, yxz[2]]
+                        if isinstance(yxz[2], list):
+                            im_subset = im_subset[:, :, 0]
+                    else:
+                        if isinstance(yxz, list):
+                            coord_index = np.ix_(yxz[0], yxz[1], yxz[2])
+                            im_subset = image[coord_index]
+                            # check shape correct
+                            self.assertTrue(im_subset.shape == tuple([len(val) for val in yxz]))
+                        else:
+                            coord_index = tuple(np.asarray(yxz[:, i]) for i in range(3))
+                            im_subset = image[coord_index]
+                            # check shape correct
+                            self.assertTrue(im_subset.shape == yxz.shape[:1])
+                if not apply_shift:
+                    im_subset = im_subset + nbp_basic.tile_pixel_value_shift
                 im_subset_load = utils.npy.load_tile(nbp_file, nbp_basic, self.t, r, channel, yxz, apply_shift)
                 diff = im_subset - im_subset_load
                 self.assertTrue(np.abs(diff).max() <= self.tol)
-            except PermissionError:
-                # In windows, can get a permission error here when trying to access temporary directory
-                self.assertTrue(True)
+        except (PermissionError, NotADirectoryError) as e:
+            # In windows, can get a permission error here when trying to access temporary directory
+            self.assertTrue(True)
 
     def test_imaging_2d(self):
         # Test can load in a whole imaging round/channel image
