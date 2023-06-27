5b76df6f443a82234611fdd78bb46afba0d57ad1
Reilly <reillytilbury@gmail.com>
2023-06-27 14:16:54 +0100

    Fixed bug regarding removed variables

--- coppafish/pipeline/omp.py
+++ coppafish/pipeline/omp.py
@@ -65,7 +65,7 @@ def call_spots_omp(config: dict, nbp_file: NotebookPage, nbp_basic: NotebookPage
     transform = jnp.asarray(transform)
     color_norm_factor = jnp.asarray(nbp_call_spots.color_norm_factor[rc_ind])
     n_genes, n_rounds_use, n_channels_use = bled_codes.shape
-    dp_norm_shift = nbp_call_spots.dp_norm_shift * np.sqrt(n_rounds_use)
+    dp_norm_shift = 0
 
     if nbp_basic.is_3d:
         detect_radius_z = config['radius_z']
@@ -181,7 +181,7 @@ def call_spots_omp(config: dict, nbp_file: NotebookPage, nbp_basic: NotebookPage
 
             pixel_coefs_tz = sparse.csr_matrix(
                 omp.get_all_coefs(pixel_colors_tz, bled_codes,
-                                  nbp_call_spots.background_weight_shift, dp_norm_shift, config['dp_thresh'],
+                                  0, dp_norm_shift, config['dp_thresh'],
                                   config['alpha'], config['beta'], config['max_genes'], config['weight_coef_fit'])[0])
             del pixel_colors_tz
             # Only keep pixels for which at least one gene has non-zero coefficient.
