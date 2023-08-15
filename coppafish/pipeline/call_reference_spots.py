2d46c7cdedb4718fcef783436ddfeac43412eaf2
Reilly <reillytilbury@gmail.com>
2023-08-15 02:27:36 +0100

    removed bug in call spots messing up omp

--- coppafish/pipeline/call_reference_spots.py
+++ coppafish/pipeline/call_reference_spots.py
@@ -134,17 +134,12 @@ def call_reference_spots(config: dict, nbp_file: NotebookPage, nbp_basic: Notebo
     nbp.gene_efficiency = gene_efficiency
 
     # Backward compatibility here as OMP requires nbp.abs_intensity_percentile.
-    # TODO: Remove this in future versions
     rc_ind = np.ix_(nbp_basic.use_rounds, nbp_basic.use_channels)
-    # pixel_colors = get_spot_colors(all_pixel_yxz(nbp_basic.tile_sz, nbp_basic.tile_sz, nbp_basic.nz // 2),
-    #                                int(np.median(nbp_basic.use_tiles)), transform, nbp_file, nbp_basic,
-    #                                return_in_bounds=True)[0]
-    # pixel_intensity = get_spot_intensity(np.abs(pixel_colors) / nbp.color_norm_factor[rc_ind])
-    # nbp.abs_intensity_percentile = np.percentile(pixel_intensity, np.arange(1, 101))
-    # nbp.gene_efficiency_intensity_thresh = float(np.round(pixel_intensity[25], 2))
-
-    # Quick Hack to get around the fact that we don't have pixel colors
-    nbp.abs_intensity_percentile = np.percentile(np.random.rand(100), np.arange(1, 101))
-    nbp.gene_efficiency_intensity_thresh = 0.5
+    pixel_colors = get_spot_colors(all_pixel_yxz(nbp_basic.tile_sz, nbp_basic.tile_sz, nbp_basic.nz // 2),
+                                   int(np.median(nbp_basic.use_tiles)), transform, nbp_file, nbp_basic,
+                                   return_in_bounds=True)[0]
+    pixel_intensity = get_spot_intensity(np.abs(pixel_colors) / nbp.color_norm_factor[rc_ind])
+    nbp.abs_intensity_percentile = np.percentile(pixel_intensity, np.arange(1, 101))
+    nbp.gene_efficiency_intensity_thresh = 0
 
     return nbp, nbp_ref_spots
