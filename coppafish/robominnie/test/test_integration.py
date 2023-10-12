7d8cdd1b9bfa8080e9a2c14dbb2c8abfc771a8b5
Paul Shuker <paul.shuker@outlook.com>
2023-10-12 13:38:29 +0100

    default parameter updates

--- coppafish/robominnie/test/test_integration.py
+++ coppafish/robominnie/test/test_integration.py
@@ -21,7 +21,7 @@ def test_integration_001() -> None:
     robominnie = RoboMinnie(include_presequence=False, include_dapi=False)
     robominnie.generate_gene_codes()
     robominnie.generate_pink_noise()
-    robominnie.add_spots(n_spots=15_000, bleed_matrix=np.diag(np.ones(7)), spot_size_pixels=np.array([1.5, 1.5, 1.5]))
+    robominnie.add_spots(n_spots=15_000)
     robominnie.save_raw_images(output_dir=output_dir, overwrite=True)
     robominnie.run_coppafish()
 
@@ -56,8 +56,8 @@ def test_integration_002() -> None:
     robominnie.generate_gene_codes()
     robominnie.generate_pink_noise()
     # Add spots to DAPI image as larger spots
-    robominnie.add_spots(n_spots=10_000, bleed_matrix=np.diag(np.ones(7)), spot_size_pixels=np.array([1.5, 1.5, 1.5]), 
-                         spot_size_pixels_dapi=np.array([9, 9, 9]), include_dapi=True, spot_amplitude_dapi=0.05)
+    robominnie.add_spots(n_spots=15_000, spot_size_pixels_dapi=np.array([9, 9, 9]), include_dapi=True, 
+                         spot_amplitude_dapi=0.05)
     # robominnie.Generate_Random_Noise(noise_mean_amplitude=0, noise_std=0.0004, noise_type='normal')
     robominnie.save_raw_images(output_dir=output_dir, overwrite=True)
     robominnie.run_coppafish()
@@ -89,13 +89,12 @@ def test_integration_003() -> None:
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
 
-    robominnie = RoboMinnie(include_presequence=False, include_dapi=False, n_tiles_x=2)
+    robominnie = RoboMinnie(n_tiles_x=2)
     robominnie.generate_gene_codes()
     robominnie.generate_pink_noise()
     # Add spots to DAPI image as larger spots
-    robominnie.add_spots(n_spots=25_000, bleed_matrix=np.diag(np.ones(7)), 
-                         spot_size_pixels=np.array([1.5, 1.5, 1.5]), include_dapi=True, 
-                         spot_size_pixels_dapi=np.array([9, 9, 9]), spot_amplitude_dapi=0.05)
+    robominnie.add_spots(n_spots=25_000, include_dapi=True, spot_size_pixels_dapi=np.array([9, 9, 9]), 
+                         spot_amplitude_dapi=0.05)
     # robominnie.generate_random_noise(noise_mean_amplitude=0, noise_std=0.0004, noise_type='normal')
     robominnie.save_raw_images(output_dir=output_dir, overwrite=True)
     robominnie.run_coppafish()
@@ -127,12 +126,11 @@ def test_bg_subtraction():
     
     rng = np.random.RandomState(0)
 
-    robominnie = RoboMinnie(include_presequence=True, include_dapi=True, 
-                            brightness_scale_factor=2 * (0.1 + rng.rand(1, 9, 8)))
+    robominnie = RoboMinnie(brightness_scale_factor=2 * (0.1 + rng.rand(1, 9, 8)))
     robominnie.generate_gene_codes()
     robominnie.generate_pink_noise()
-    robominnie.add_spots(n_spots=15_000, bleed_matrix=np.diag(np.ones(7)), spot_size_pixels=np.array([1.5, 1.5, 1.5]),
-                         gene_efficiency=0.5 * (rng.rand(15, 8) + 1), background_offset=1e-7*rng.rand(15_000, 7))
+    robominnie.add_spots(n_spots=15_000, gene_efficiency=0.5 * (rng.rand(15, 8) + 1), 
+                         background_offset=1e-7*rng.rand(15_000, 7))
     robominnie.save_raw_images(output_dir=output_dir, overwrite=True)
     robominnie.run_coppafish()
 
@@ -150,6 +148,5 @@ def test_bg_subtraction():
         warnings.warn(UserWarning('Integration test passed, but the overall OMP spots score is < 75%'))
 
 
-
 if __name__ == '__main__':
-    test_bg_subtraction()
+    test_integration_003()
