94d49f20f5f995f0430811c25d8f6197fa01f06c
Paul Shuker <paul.shuker@outlook.com>
2023-10-16 10:58:12 +0100

    no dapi register integration test add

--- coppafish/robominnie/test/test_integration.py
+++ coppafish/robominnie/test/test_integration.py
@@ -8,11 +8,9 @@ import pytest
 @pytest.mark.slow
 def test_integration_001() -> None:
     """
-    Summary of input data: random spots and random, white noise.
+    Summary of input data: random spots and pink noise.
 
     Includes anchor round, sequencing rounds, one tile.
-
-    Compares ground truth spots to OMP spots and reference spots.
     """
     output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'integration_dir')
     if not os.path.isdir(output_dir):
@@ -37,16 +35,15 @@ def test_integration_001() -> None:
     print(f'Overall score: {round(overall_score*100, 1)}%')
     if overall_score < 0.75:
         warnings.warn(UserWarning('Integration test passed, but the overall OMP spots score is < 75%'))
+    del robominnie
 
 
 @pytest.mark.slow
 def test_integration_002() -> None:
     """
-    Summary of input data: random spots and random, white noise.
+    Summary of input data: random spots and pink noise.
 
     Includes anchor round, DAPI image, presequence round, sequencing rounds, one tile.
-
-    Compares ground truth spots to OMP spots and reference spots.
     """
     output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'integration_dir')
     if not os.path.isdir(output_dir):
@@ -74,16 +71,15 @@ def test_integration_002() -> None:
     print(f'Overall score: {round(overall_score*100, 1)}%')
     if overall_score < 0.75:
         warnings.warn(UserWarning('Integration test passed, but the overall OMP spots score is < 75%'))
+    del robominnie
 
 
 @pytest.mark.slow
 def test_integration_003() -> None:
     """
-    Summary of input data: random spots and random, white noise.
+    Summary of input data: random spots and pink noise.
 
     Includes anchor, DAPI, presequencing round and sequencing rounds, `2` connected tiles, aligned along the x axis.
-
-    Compares ground truth spots to OMP spots and reference spots.
     """
     output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'integration_dir')
     if not os.path.isdir(output_dir):
@@ -116,6 +112,43 @@ def test_integration_003() -> None:
     print(f'Overall score: {round(overall_score*100, 1)}%')
     if overall_score < 0.75:
         warnings.warn(UserWarning('Integration test passed, but the overall OMP spots score is < 75%'))
+    del robominnie
+
+
+@pytest.mark.slow
+def test_integration_004():
+    """
+    Summary of input data: random spots and pink noise.
+    
+    Includes anchor round, DAPI image, presequence round, sequencing rounds, one tile. No DAPI channel registration.
+    """
+    output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'integration_dir')
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+
+    robominnie = RoboMinnie()
+    robominnie.generate_gene_codes()
+    robominnie.generate_pink_noise()
+    # Add spots to DAPI image as larger spots
+    robominnie.add_spots(n_spots=15_000, spot_size_pixels_dapi=np.array([9, 9, 9]), include_dapi=True, 
+                         spot_amplitude_dapi=0.05)
+    # robominnie.Generate_Random_Noise(noise_mean_amplitude=0, noise_std=0.0004, noise_type='normal')
+    robominnie.save_raw_images(output_dir=output_dir, overwrite=True, register_with_dapi=False)
+    robominnie.run_coppafish()
+
+    robominnie.compare_spots('ref')
+    overall_score = robominnie.overall_score()
+    print(f'Overall score: {round(overall_score*100, 1)}%')
+    if overall_score < 0.75:
+        warnings.warn(UserWarning('Integration test passed, but the overall OMP spots score is < 75%'))
+    assert overall_score > 0.5, 'Integration reference spots score < 50%!'
+
+    robominnie.compare_spots('omp')
+    overall_score = robominnie.overall_score()
+    print(f'Overall score: {round(overall_score*100, 1)}%')
+    if overall_score < 0.75:
+        warnings.warn(UserWarning('Integration test passed, but the overall OMP spots score is < 75%'))
+    del robominnie
 
 
 @pytest.mark.slow
@@ -146,7 +179,8 @@ def test_bg_subtraction():
     print(f'Overall score: {round(overall_score*100, 1)}%')
     if overall_score < 0.75:
         warnings.warn(UserWarning('Integration test passed, but the overall OMP spots score is < 75%'))
+    del robominnie
 
 
 if __name__ == '__main__':
-    test_integration_003()
+    test_integration_001()
