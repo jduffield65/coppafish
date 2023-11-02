9279aad00d2d2e1e2914fcd67ab2a8640ac22071
Paul Shuker <paul.shuker@outlook.com>
2023-11-02 16:07:31 +0000

    cpu fix

--- coppafish/omp/coefs_optimised.py
+++ coppafish/omp/coefs_optimised.py
@@ -10,7 +10,8 @@ from .. import call_spots
 from ..setup import NotebookPage
 from ..call_spots import dot_product_optimised
 
-os.environ['XLA_FLAGS'] = f'--xla_force_host_platform_device_count={utils.threads.get_available_threads()}'
+if jax.default_backend() == 'cpu':
+    os.environ['XLA_FLAGS'] = f'--xla_force_host_platform_device_count={utils.threads.get_available_threads()}'
 import jax.numpy as jnp
 import jax
 
