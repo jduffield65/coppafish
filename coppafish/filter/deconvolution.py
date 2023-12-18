pixel unique values/counts in filter + psf calculation image fix

coppafish/filter/deconvolution.py
@@ -30,7 +30,7 @@ def psf_pad(psf: np.ndarray, image_shape: Union[np.ndarray, List[int]]) -> np.nd
 def get_psf_spots(nbp_file: NotebookPage, nbp_basic: NotebookPage, nbp_extract: NotebookPage, round: int,
                   use_tiles: List[int], channel: int, use_z: List[int], radius_xy: int, radius_z: int, min_spots: int,
                   intensity_thresh: Optional[float], intensity_auto_param: float, isolation_dist: float,
-                  shape: List[int], image_trc_raw: np.ndarray = None) -> Tuple[np.ndarray, float, List[int]]:
+                  shape: List[int]) -> Tuple[np.ndarray, float, List[int]]:
     """
     Finds spot_shapes about spots found in raw data, average of these then used for psf.
 
@@ -52,8 +52,6 @@ def get_psf_spots(nbp_file: NotebookPage, nbp_basic: NotebookPage, nbp_extract: 
         intensity_auto_param: If ```intensity_thresh = None``` so is automatically computed, it is done using this.
         isolation_dist: Spots are isolated if nearest neighbour is further away than this.
         shape: ```int [y_diameter, x_diameter, z_diameter]```. Desired size of image about each spot.
-        image_trc_raw (`(nz x ny x nx) ndarray[uint16]`, optional): raw image for `round` and `channel`. Default: not 
-            given.
 
     Returns:
         - ```spot_images``` - ```int [n_spots x y_diameter x x_diameter x z_diameter]```.
@@ -70,16 +68,10 @@ def get_psf_spots(nbp_file: NotebookPage, nbp_basic: NotebookPage, nbp_extract: 
 
             rda, _ = utils.raw.load_dask(nbp_file, nbp_basic, r=round)
             # choose tile closest to centre
-            if image_trc_raw is None:
-                im = utils.tiles_io._load_image(nbp_file.tile_unfiltered[t][round][channel], nbp_extract.file_type)
-            else:
-                im = image_trc_raw[use_z]
+            im = utils.tiles_io._load_image(nbp_file.tile_unfiltered[t][round][channel], nbp_extract.file_type)
         else:
             t = scale.base.central_tile(nbp_basic.tilepos_yx, use_tiles)  # choose tile closet to centre
-            if image_trc_raw is None:
-                im = utils.tiles_io._load_image(nbp_file.tile_unfiltered[t][round][channel], nbp_extract.file_type)
-            else:
-                im = image_trc_raw[use_z]
+            im = utils.tiles_io._load_image(nbp_file.tile_unfiltered[t][round][channel], nbp_extract.file_type)
         # zyx -> yxz
         im = im.transpose((1, 2, 0))
         del image_trc_raw

coppafish/pipeline/extract_run.py
@@ -93,7 +93,7 @@ def run_extract(
         )
     else:
         image_t = None
-    all_pixel_unique_values = np.full(
+    nbp_debug.pixel_unique_values = np.full(
         (
             nbp_basic.n_tiles,
             nbp_basic.n_rounds + nbp_basic.n_extra_rounds,
@@ -103,7 +103,7 @@ def run_extract(
         fill_value=0,
         dtype=int, 
     )
-    all_pixel_unique_counts = all_pixel_unique_values.copy()
+    nbp_debug.pixel_unique_counts = nbp_debug.pixel_unique_values.copy()
 
     with tqdm(
         total=n_images, desc=f"Loading raw {nbp_file.raw_extension} tiles and saving as {config['file_type']}"
@@ -160,12 +160,10 @@ def run_extract(
                         if return_image_t:
                             image_t[r, c] = im
                         pixel_unique_values, pixel_unique_counts = np.unique(im, return_counts=True)
-                        all_pixel_unique_values[t][r][c][: pixel_unique_values.size] = pixel_unique_values
-                        all_pixel_unique_counts[t][r][c][: pixel_unique_counts.size] = pixel_unique_counts
+                        nbp_debug.pixel_unique_values[t][r][c][: pixel_unique_values.size] = pixel_unique_values
+                        nbp_debug.pixel_unique_counts[t][r][c][: pixel_unique_counts.size] = pixel_unique_counts
                         del im
                         pbar.update(1)
     end_time = time.time()
-    nbp_debug.pixel_unique_values = all_pixel_unique_values
-    nbp_debug.pixel_unique_counts = all_pixel_unique_counts
     nbp_debug.time_taken = end_time - start_time
     return nbp, nbp_debug, image_t

coppafish/pipeline/filter_run.py
@@ -111,6 +111,17 @@ def run_filter(
     # initialise debugging info as 'debug' page
     nbp_debug.n_clip_pixels = np.zeros_like(nbp.auto_thresh, dtype=int)
     nbp_debug.clip_extract_scale = np.zeros_like(nbp.auto_thresh)
+    nbp_debug.pixel_unique_values = np.full(
+        (
+            nbp_basic.n_tiles,
+            nbp_basic.n_rounds + nbp_basic.n_extra_rounds,
+            nbp_basic.n_channels,
+            np.iinfo(np.uint16).max,
+        ),
+        fill_value=0,
+        dtype=int, 
+    )
+    nbp_debug.pixel_unique_counts = nbp_debug.pixel_unique_values.copy()
 
     # If we have a pre-sequencing round, add this to round_files at the end
     if nbp_basic.use_preseq:
@@ -163,7 +174,6 @@ def run_filter(
                 config["auto_thresh_multiplier"],
                 config["psf_isolation_dist"],
                 config["psf_shape"],
-                image_t_raw[nbp_basic.anchor_round, nbp_basic.anchor_channel], 
             )
             psf = deconvolution.get_psf(spot_images, config["psf_annulus_width"])
             np.save(nbp_file.psf, np.moveaxis(psf, 2, 0))  # save with z as first axis
@@ -382,6 +392,9 @@ def run_filter(
                             )
                             if return_image_t:
                                 image_t[r, c] = saved_im
+                            pixel_unique_values, pixel_unique_counts = np.unique(saved_im, return_counts=True)
+                            nbp_debug.pixel_unique_values[t][r][c][: pixel_unique_values.size] = pixel_unique_values
+                            nbp_debug.pixel_unique_counts[t][r][c][: pixel_unique_counts.size] = pixel_unique_counts
                             del saved_im
                         else:
                             im_all_channels_2d[c] = im