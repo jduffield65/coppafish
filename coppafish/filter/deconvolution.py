limiting spot count in psf calculation fix

coppafish/filter/deconvolution.py
@@ -87,15 +87,16 @@ def get_psf_spots(nbp_file: NotebookPage, nbp_basic: NotebookPage, nbp_extract: 
         isolated = find_spots.get_isolated_points(
             spot_yxz * [1, 1, nbp_basic.pixel_size_z / nbp_basic.pixel_size_xy], isolation_dist, 
         )
-        if maximum_spots is not None and np.sum(isolated) > maximum_spots:
+        chosen_spots = np.logical_and(isolated, not_single_pixel)
+        if maximum_spots is not None and np.sum(chosen_spots) > maximum_spots:
             n_isolated_spots = 0
-            for i in range(isolated.shape[0]):
+            for i in range(chosen_spots.shape[0]):
                 if n_isolated_spots == maximum_spots:
-                    isolated[i:] = False
+                    chosen_spots[i:] = False
                     break
-                if isolated[i]:
+                if chosen_spots[i]:
                     n_isolated_spots += 1
-        spot_yxz = spot_yxz[np.logical_and(isolated, not_single_pixel), :]
+        spot_yxz = spot_yxz[chosen_spots, :]
         if n_spots == 0 and np.shape(spot_yxz)[0] < min_spots / 4:
             # raise error on first tile if looks like we are going to use more than 4 tiles
             raise ValueError(f"\nFirst tile, {t}, only found {np.shape(spot_yxz)[0]} spots."